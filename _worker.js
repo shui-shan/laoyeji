7fe62484448a97e9bae70913b1dec5c64d38c5de
shui-shan <100398035+shui-shan@users.noreply.github.com>
2025-04-12 14:31:12 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -10,7 +10,7 @@ let SUBUpdateTime = 6; //更改此次设置订阅更新时间，单位小时
 
 //在以下输入您的自建节点链接或订阅链接，请保留双引号
 let MainData = `
-vless://5a6f3af1-f125-432c-98bc-c6faaf9ad363@47.76.194.126:56341?type=tcp&security=reality&sni=252de87f.com&pbk=Z3ZGnAOdKkzJ07gR_7_0k9_iTTFP6paDOrqx1rN2LU4&flow=xtls-rprx-vision&fp=chrome#【alice+claw】香港-老爷机专用
+vless://6759f41c-dd8f-49e4-8d61-3bbb19afdedf@aliyun.shuishan.us.kg:56341?type=tcp&security=reality&pbk=dOAbn7po8IoME9LX1608vk7-f_2tynYpW8U3JN6WakE&fp=chrome&sni=1942728e.com&sid=a0d0a689851ac18b&spx=%2F&flow=xtls-rprx-vision#%E3%80%90claw%2Bakile%E3%80%91%E9%A6%99%E6%B8%AF-%E8%80%81%E7%88%B7%E6%9C%BA%E4%B8%93%E7%94%A8
 `
 
 //请将机场订阅链接填入上方
